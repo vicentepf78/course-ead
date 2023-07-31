baa46ede8cc1fabdd573cda6ceab3b4403252ec0
vicentepf78 <vicentepf78.gmail.com>
2023-07-31 16:02:06 -0300

    Criando projeto MS course + Spring Logging nos Microservices

---
+++ src/main/java/com/ead/course/controllers/CourseController.java
@@ -0,0 +1,95 @@
+package com.ead.course.controllers;
+
+import com.ead.course.dtos.CourseDto;
+import com.ead.course.models.CourseModel;
+import com.ead.course.services.CourseService;
+import com.ead.course.specification.SpecificationTemplate;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Optional;
+import java.util.UUID;
+
+@Log4j2
+@RestController
+@RequestMapping("/courses")
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class CourseController {
+
+    @Autowired
+    private CourseService courseService;
+
+    @PostMapping
+    public ResponseEntity<Object> saveCourse(@RequestBody @Valid CourseDto courseDto){
+        log.debug("POST saveCourse courseDto received {} ", courseDto.toString());
+        var courseModel = new CourseModel();
+        BeanUtils.copyProperties(courseDto, courseModel);
+        courseModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
+        courseModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+        courseService.save(courseModel);
+        log.debug("POST saveCourse courseId saved {} ", courseModel.getCourseId());
+        log.info("Course saved successfully courseId {} ", courseModel.getCourseId());
+        return ResponseEntity.status(HttpStatus.CREATED).body(courseModel);
+    }
+
+
+    @DeleteMapping("/{courseId}")
+    public ResponseEntity<Object> deleteCourse(@PathVariable(value="courseId") UUID courseId){
+        log.debug("DELETE deleteCourse courseId received {} ", courseId);
+        Optional<CourseModel> courseModelOptional = courseService.findById(courseId);
+        if(!courseModelOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Course Not Found.");
+        }
+        courseService.delete(courseModelOptional.get());
+        log.debug("DELETE deleteCourse courseId deleted {} ", courseId);
+        log.info("Course deleted successfully courseId {} ", courseId);
+        return ResponseEntity.status(HttpStatus.OK).body("Course deleted successfully.");
+    }
+
+
+    @PutMapping("/{courseId}")
+    public ResponseEntity<Object> updateCourse(@PathVariable(value="courseId") UUID courseId,
+                                               @RequestBody @Valid CourseDto courseDto){
+        log.debug("PUT updateCourse courseDto received {} ", courseDto.toString());
+        Optional<CourseModel> courseModelOptional = courseService.findById(courseId);
+        if(!courseModelOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Course Not Found.");
+        }
+        var courseModel = courseModelOptional.get();
+        BeanUtils.copyProperties(courseDto, courseModel);
+        courseModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+        courseService.save(courseModel);
+        log.debug("PUT updateCourse courseId saved {} ", courseModel.getCourseId());
+        log.info("Course updated successfully courseId {} ", courseModel.getCourseId());
+        return ResponseEntity.status(HttpStatus.OK).body(courseModel);
+    }
+
+
+    @GetMapping
+    public ResponseEntity<Page<CourseModel>> getAllCourses(SpecificationTemplate.CourseSpec spec,
+                                                           @PageableDefault(page = 0, size = 10, sort = "courseId", direction = Sort.Direction.ASC) Pageable pageable){
+        return ResponseEntity.status(HttpStatus.OK).body(courseService.findAll(spec, pageable));
+    }
+
+    @GetMapping("/{courseId}")
+    public ResponseEntity<Object> getOneCourse(@PathVariable(value="courseId") UUID courseId){
+        Optional<CourseModel> courseModelOptional = courseService.findById(courseId);
+        if(!courseModelOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Course Not Found.");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(courseModelOptional.get());
+    }
+
+
+}
